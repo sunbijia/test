ab96e75015804907a1d7528fd90199502adc82d1
sbj <978851607@qq.com>
2018-07-18 16:08:59 +0800

    build修改

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 mvn clean
-cd test01
 mvn package
-cd target
-java -jar app_test01_filename.jar
-cd ..
-cd ..
-cd test02
-mvn package
-cd target
-java -jar app_test02_filename.jar
+java -jar test01/target/app_test01_filename.jar
+java -jar test02/target/app_test02_filename.jar
