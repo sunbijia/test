1d5baa71d36d567ad8f549409330976700ba5847
sbj <978851607@qq.com>
2018-07-18 14:58:56 +0800

    test

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 mvn clean
+cd test01
 mvn package
 cd target
-java -jar app_test_filename.jar
+java -jar app_test01_filename.jar
+cd ..
+cd ..
+cd test02
+mvn package
+cd target
+java -jar app_test02_filename.jar
---
+++ test02/src/main/java/com/hand/test02.java
@@ -0,0 +1,7 @@
+package com.hand;
+
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("this is test02");
+    }
+}
